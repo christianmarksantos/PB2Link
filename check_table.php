0a9f1b8e34eaf8f5f288efe3f1346fca93166074
CHRISTIAN MARK D. SANTOS <christianmark.santos@cvsu.edu.ph>
2026-05-16 18:25:01 +0800

    Delete check_table.php

--- check_table.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db_connection.php';
-
-$result = mysqli_query($conn, 'DESCRIBE users');
-echo "Users table structure:\n";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['Field'] . ' - ' . $row['Type'] . ' - ' . ($row['Null'] == 'YES' ? 'NULL' : 'NOT NULL') . "\n";
-}
-?>
